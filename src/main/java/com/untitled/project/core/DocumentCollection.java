fca58d576ecd5ac9efbdfda1d0650ba107b9ef5b
Aman Rao <amanrao032@gmail.com>
2025-11-14 20:03:07 +0530

    Removed abstract methods that shouldn't be part of the entity

--- src/main/java/com/untitled/project/core/DocumentCollection.java
+++ src/main/java/com/untitled/project/core/DocumentCollection.java
@@ -1,12 +1,9 @@
 package com.untitled.project.core;
 
 import com.untitled.project.core.identifier.DocumentCollectionIdentifier;
-import com.untitled.project.core.identifier.DocumentIdentifier;
 
 public abstract class DocumentCollection<T, U, V extends DocumentCollectionIdentifier<U>> {
     protected final V id;
-    public abstract boolean addDocument(Document<T> document);
-    public abstract boolean deleteDocument(DocumentIdentifier<T> documentIdentifier);
 
     public DocumentCollection(V id) {
         this.id = id;
