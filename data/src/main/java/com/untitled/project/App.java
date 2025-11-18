f7552c707bd5a02e1c406c97f13c7c5060e56f33
Aman Rao <amanrao032@gmail.com>
2025-11-19 00:19:18 +0530

    setup flyway for db migrations

---
+++ data/src/main/java/com/untitled/project/App.java
@@ -0,0 +1,13 @@
+package com.untitled.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
